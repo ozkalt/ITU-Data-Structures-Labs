3c2b495add8bb6174a183402c11f6c5b3acec7c2
Tuğba Özkal <ozkalt@itu.edu.tr>
2017-10-11 12:45:08 +0300

    structs updated

--- Lab4/node.h
+++ Lab4/node.h
@@ -2,14 +2,9 @@
 #define PHONENUM_LENGTH 15
 #define TYPE_LENGTH 7
 
-struct number{
-    char type[TYPE_LENGTH];
-    char num[PHONENUM_LENGTH];
-    number *next;
-};
-
 struct Phone_node{
     char *name;
-    number *phonenum;
+    char *lastname;
+    char *phonenum;
     Phone_node *next;
 };
